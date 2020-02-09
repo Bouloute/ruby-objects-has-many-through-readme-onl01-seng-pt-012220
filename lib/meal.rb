31ab1997a68759a770c91d769666654b89c6f141
Anna Lewis Dagnino <annaick1995@gmail.com>
2020-02-09 22:44:17 +0000

    Automatically backed up by Learn

--- lib/meal.rb
+++ lib/meal.rb
@@ -2,6 +2,9 @@ class Meal
   attr_reader :waiter, :customer, :total, :tip
 
   def initialize(waiter, customer, total, tip)
-
+    @waiter = waiter
+    @customer = customer
+    @total = total
+    @tip = tip
   end
 end
